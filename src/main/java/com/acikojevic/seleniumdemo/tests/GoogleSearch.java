e733cff8d014773f1f8c2ce37893469dcab825d5
acikojevic <ante.cikojevic@infobip.com>
2017-07-23 01:02:35 +0200

    test

--- src/main/java/com/acikojevic/seleniumdemo/tests/GoogleSearch.java
+++ src/main/java/com/acikojevic/seleniumdemo/tests/GoogleSearch.java
@@ -2,5 +2,5 @@ package com.acikojevic.seleniumdemo.tests;
 
 import com.acikojevic.seleniumdemo.core.TestBase;
 
-public class GoogleSearch extends TestBase{
+public class GoogleSearch extends TestBase {
 }
