6da193f5e3be91844a4f66933b7b5c1a7b62c971
acikojevic <ante.cikojevic@gmail.com>
2017-08-20 00:53:48 +0200

    basic classes and methods created + basic test and testNG.xml

--- src/main/java/com/acikojevic/seleniumdemo/tests/GoogleSearch.java
+++ src/main/java/com/acikojevic/seleniumdemo/tests/GoogleSearch.java
@@ -1,6 +1,18 @@
 package com.acikojevic.seleniumdemo.tests;
 
 import com.acikojevic.seleniumdemo.core.TestBase;
+import com.acikojevic.seleniumdemo.pageobjectpages.GoogleSearchPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class GoogleSearch extends TestBase{
+public class GoogleSearch extends TestBase {
+
+    @Test
+    public void googleSearchTest() {
+
+        driver.get("https://www.google.com");
+        GoogleSearchPage googleSearchPage = new GoogleSearchPage(driver);
+        googleSearchPage.searchByText("nekitekst");
+        Assert.assertTrue(googleSearchPage.isSearchResultFound());
+    }
 }
